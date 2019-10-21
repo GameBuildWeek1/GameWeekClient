3474d53ef191c1f3654dc3e86311e4e3b7a01d9f
Alan Perez <alanp9six@gmail.com>
2019-10-21 15:33:52 -0500

    :tada: Creating Login Component

---
+++ client/src/components/Login.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+
+function Login(props) {
+  
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
