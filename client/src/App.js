ff1442902388038d13e7f0fc37d575de6fa1310c
Alan Perez <alanp9six@gmail.com>
2019-10-22 12:55:10 -0500

    removed sass var.. and added login component..

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 //import { Route } from "react-router-dom";
+import Login from '../src/components/Login'
+import Register from '../src/components/Register'
+function App() {
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>hello world</h1>
-      </div>
-    );
-  }
+  const[isLoggedIn, setIsLoggedIn] = useState (false)
+
+  return (
+    <div className="App">
+      <h2>Welcome!</h2>
+      <h2>Login</h2>
+      <Login />
+      <h2>Register</h2>
+      <Register />
+    </div>
+  )
 }
 
 export default App;
